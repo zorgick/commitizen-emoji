e4c5d24aefb1fa9160bcf18b7aa9cd91c91dbb5b
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-20 23:34:29 +0300

    🔧  config: update jest config with ts and msw support

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,18 @@
 import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+
+// Load the config which holds the path aliases.
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      // This has to match the baseUrl defined in tsconfig.json.
+      prefix: '<rootDir>/',
+    }),
+  },
+  setupFilesAfterEnv: ['./src/tests/mocks/setupEnv.ts']
 };
 
 export default config;
