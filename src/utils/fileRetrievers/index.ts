a2a133d1013a31d47618fac6e1ea237529015f21
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-22 22:41:40 +0300

    ✨  feature: add util for loading girmojis and test it

--- src/utils/fileRetrievers/index.ts
+++ src/utils/fileRetrievers/index.ts
@@ -1,1 +1,2 @@
 export * from './loadLocalFile';
+export * from './loadGitmoji';
---
+++ src/utils/fileRetrievers/loadGitmoji.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import util from 'util';
+import path from 'path';
+import fetch from 'node-fetch';
+
+import {
+  LoadGitmojiType,
+  GitmojiObjectType,
+  ResponseGitmojiType,
+} from 'types'
+
+import {
+  loadLocalFile,
+} from './index'
+
+const writeFile = util.promisify(fs.writeFile);
+
+/** 
+ * This function loads up-to-date version of gitmoji file via network
+ * OR uses local version if it exists
+ * @return {Object} gitmoji-types object
+ */
+export const loadGitmoji: LoadGitmojiType = async () => {
+  const filePath = path.join(__dirname, '..', '..', 'data', 'gitmojis.json');
+  let gitmojiTypes: GitmojiObjectType[] = [];
+
+  if (fs.existsSync(filePath)) {
+    // Read contents from existing file
+    gitmojiTypes = await loadLocalFile<GitmojiObjectType[]>(filePath);
+  } else {
+    // or download gitmojis.json, if it doesn't exist yet
+    try {
+      const gitmojiJson: ResponseGitmojiType = await fetch('https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
+        .then(res => res.json());
+      gitmojiTypes = gitmojiJson.gitmojis;
+      await writeFile(filePath, JSON.stringify(gitmojiTypes, undefined, 2));
+    } catch (e) {
+      /* istanbul ignore next */
+      throw Error(
+        'Failed to fetch gitmoji JSON, please refer to https://github.com/zorgick/commitizen-emoji/issues for help.',
+      );
+    }
+  }
+  return gitmojiTypes;
+};
+
