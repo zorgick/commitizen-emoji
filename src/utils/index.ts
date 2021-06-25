ae398ac5072e15a722e45e1b2b3de88376e7cf45
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-25 21:55:02 +0300

    ✨  feature: add util for validation of user type names and run tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,10 @@
 export * from './fileRetrievers';
+export * from './validators';
 // exports for tests
 import * as fileRetrievers from './fileRetrievers';
+import * as validators from './validators';
 
 export default {
   ...fileRetrievers,
+  ...validators,
 }
---
+++ src/utils/validators/validateUserTypeName.ts
@@ -0,0 +1,20 @@
+import {
+  ValidateUserTypeNameType,
+} from '../../types'
+
+import {
+  ERROR_TYPE_NAME_FORMAT,
+  ERROR_MISSING_EMOJI_CODE,
+} from 'consts';
+
+export const validateUserTypeName: ValidateUserTypeNameType = ([code, name], defaultTypeNames) => {
+  // typescript assumes that entries returns array of string pairs
+  // but in fact second value can be of any type
+  if (typeof name !== 'string') {
+    throw new Error(ERROR_TYPE_NAME_FORMAT(name));
+  }
+
+  if (!defaultTypeNames.has(code)) {
+    throw new Error(ERROR_MISSING_EMOJI_CODE(code));
+  }
+};
