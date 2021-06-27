9f2e4cd3c312815a4d0b83a1b1f330554d846c1b
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-27 19:54:15 +0300

    🏷️  types: change error function argument type

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -2,7 +2,7 @@ export const ERROR_TYPE_NAME_FORMAT = (name: any) =>
   `Provided type name "${name}" is not a string.
 Please, provide a correct string type.`
 
-export const ERROR_MISSING_EMOJI_CODE = (code: string) =>
+export const ERROR_MISSING_EMOJI_CODE = (code: any) =>
   `Provided emoji code "${code}" doesn't exist in gitmoji type codes.
 Please, check the code name or its format.`
 
