de59d381e50abc4c2e1f78dc24cb86b315a847a4
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-27 10:38:14 +0300

    ✨  feature(tdd): add util to map type names with emoji codes; run tests

---
+++ src/utils/combiners/mappers.ts
@@ -0,0 +1,36 @@
+import {
+  TYPE_NAMES,
+} from 'consts';
+import {
+  MapTypeNamesType,
+  CodeNamesType,
+} from 'types'
+
+import {
+  validateUserTypeName,
+} from '../index'
+
+export const mapTypeNames: MapTypeNamesType = (gitmojiTypes, userTypeNames) => {
+  const defaultPreferedTypeNames = new Map(TYPE_NAMES);
+  type DefaultsMapType = typeof defaultPreferedTypeNames;
+
+  // if user type names are given we need to replace
+  // default type names with user ones
+  if (userTypeNames) {
+    Object.entries(userTypeNames).forEach(([code, name]: [string, any]) => {
+      // this will throw if user emoji code or type name are corrupted
+      // (non-existant or of a wrong format)
+      validateUserTypeName<DefaultsMapType>([code, name], defaultPreferedTypeNames);
+
+      // set user defined type names to gitmoji codes.
+      // Name is in fact of type string, but ts accepts only literals here
+      // from default TYPE_NAMES
+      defaultPreferedTypeNames.set(code as CodeNamesType, name as any);
+    });
+  }
+
+  return gitmojiTypes.map(gitMojiObject => ({
+    ...gitMojiObject,
+    name: defaultPreferedTypeNames.get(gitMojiObject.code) as string,
+  }));
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,13 @@
 export * from './fileRetrievers';
+export * from './combiners';
 export * from './validators';
 // exports for tests
 import * as fileRetrievers from './fileRetrievers';
+import * as combiners from './combiners';
 import * as validators from './validators';
 
 export default {
   ...fileRetrievers,
+  ...combiners,
   ...validators,
 }
