c282efa9e0314f96d6205151179d30d37af6919d
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-30 00:28:33 +0300

    🏷️  types: add types for validation of user types list

--- src/types/typesValidators.ts
+++ src/types/typesValidators.ts
@@ -1,17 +1,21 @@
 import {
   MapInstance,
   CodeNamesType,
+  CommonObjectType,
 } from './index';
 
 // DTOs
 
 // Functions
 
+export type ValidateUserTypeListType = (
+  typeList: CommonObjectType[],
+) => void;
 export type ValidateUserTypeNameType = <T extends MapInstance<any, any>>(
   emojiPair: [string, any],
   defaultTypeNames: T,
 ) => void;
-export type ValidateSelectedEmojiCodesType = <T extends MapInstance<any, any>>(
+export type ValidateSelectedEmojiCodesType = (
   emojiCode: any,
   defaultCodeNames: CodeNamesType[],
 ) => void;
