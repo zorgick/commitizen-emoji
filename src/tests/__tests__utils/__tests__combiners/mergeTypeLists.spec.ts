efa3bda77fd474bc3d31c8a5b7caf705468b80db
Zackary Kononov <zorgizorgik@gmail.com>
2021-06-30 22:36:29 +0300

    🧪  fall(tdd): add tests for merging type lists

---
+++ src/tests/__tests__utils/__tests__combiners/mergeTypeLists.spec.ts
@@ -0,0 +1,94 @@
+import utils from 'utils';
+import { gitmojis } from '../../fixtures/testFile.json'
+
+test('returns default gitmoji types appended with user emoji types', () => {
+  const userTypes = [
+    {
+      emoji: "=)",
+      code: ":happy:",
+      description: "Happy test.",
+      name: "happy",
+    },
+    {
+      emoji: "=(",
+      code: ":sad:",
+      description: "Sad test.",
+      name: "sad",
+    },
+  ];
+  const result = utils.mergeTypeLists(gitmojis, userTypes);
+  expect(result).toEqual([...gitmojis, ...userTypes]);
+})
+
+test('returns default gitmoji types with inserted user emoji types', () => {
+  const gitmojisTypes = [...gitmojis];
+  const userTypes = [
+    {
+      emoji: "=)",
+      code: ":art:",
+      description: "Happy test.",
+      name: "happy",
+    },
+    {
+      emoji: "=(",
+      code: ":coffin:",
+      description: "Sad test.",
+      name: "sad",
+    },
+  ];
+  const result = utils.mergeTypeLists(gitmojis, userTypes);
+  const artIndex = gitmojisTypes.findIndex(({ code }) => code === ':art:');
+  const coffinIndex = gitmojisTypes.findIndex(({ code }) => code === ':coffin:');
+  // @ts-ignore
+  gitmojisTypes[artIndex] = userTypes[0];
+  // @ts-ignore
+  gitmojisTypes[coffinIndex] = userTypes[1];
+  expect(result).toEqual(gitmojisTypes);
+})
+
+test('returns default gitmoji types with inserted and added user emoji types', () => {
+  const gitmojisTypes = [...gitmojis];
+  const userTypes = [
+    {
+      emoji: "=)",
+      code: ":art:",
+      description: "Happy test.",
+      name: "happy",
+    },
+    {
+      emoji: "=(",
+      code: ":sad:",
+      description: "Sad test.",
+      name: "sad",
+    },
+  ];
+  const result = utils.mergeTypeLists(gitmojis, userTypes);
+  const artIndex = gitmojisTypes.findIndex(({ code }) => code === ':art:');
+  // @ts-ignore
+  gitmojisTypes[artIndex] = userTypes[0];
+  expect(result).toEqual([...gitmojisTypes, userTypes[1]]);
+})
+
+test('inserts the latest user type of the same code', () => {
+  const gitmojisTypes = gitmojis.slice(0, 3);
+  const userTypes = [
+    {
+      emoji: "=)",
+      code: ":zap:",
+      description: "Happy test.",
+      name: "happy",
+    },
+    {
+      emoji: "=(",
+      code: ":zap:",
+      description: "Sad test.",
+      name: "sad",
+    },
+  ];
+  const result = utils.mergeTypeLists(gitmojisTypes, userTypes);
+  const zapIndex = gitmojisTypes.findIndex(({ code }) => code === ':zap:');
+  // @ts-ignore
+  gitmojisTypes[zapIndex] = userTypes[1];
+  expect(result).toHaveLength(3);
+  expect(result).toEqual(gitmojisTypes);
+})
