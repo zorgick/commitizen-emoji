fcf29d9136ab2715547f0d371fbc4fdb5eb13461
Zackary Kononov <zorgizorgik@gmail.com>
2021-07-05 09:30:34 +0300

    ✅  tests(tdd): fix format issues tests to accept general prefix

--- src/__tests__/formatters/formatIssues.spec.ts
+++ src/__tests__/formatters/formatIssues.spec.ts
@@ -11,10 +11,11 @@ const setup = (adjustedConfig?: Partial<ConfigType>) => {
 
   return {
     config,
+    prefix: 'See issues: ',
   };
 }
 
-test('returns empty string if issues are empty', () => {
+test('returns an empty string if issues are empty', () => {
   const url = 'https://test.com';
   const { config } = setup({
     issuesPrefix: url
@@ -24,41 +25,44 @@ test('returns empty string if issues are empty', () => {
 })
 
 test('returns issues numbers without prefix (user uses coma)', () => {
-  const { config } = setup()
+  const { config, prefix } = setup()
   const testIssues = '13123-asf123, 123123, ass1jJiasd123';
-  expect(lib.formatIssues(testIssues, config)).toEqual(testIssues);
+  expect(lib.formatIssues(testIssues, config)).toEqual(prefix + testIssues);
 })
 
 test('returns issues numbers without prefix (user uses blank space)', () => {
-  const { config } = setup()
+  const { config, prefix } = setup()
   const testIssues = '13123-asf123 123123 ass1jJiasd123';
-  expect(lib.formatIssues(testIssues, config)).toEqual(testIssues.split(' ').join(', '));
+  expect(lib.formatIssues(testIssues, config))
+    .toEqual(prefix + testIssues.split(' ').join(', '));
 })
 
 test('returns issues numbers prepended with a prefix as a valid url (from root)', () => {
   const url = 'https://test.com';
-  const { config } = setup({
+  const { config, prefix } = setup({
     issuesPrefix: url
   })
   const testIssues = '13123-asf123 123123 ass1jJiasd123';
-  expect(lib.formatIssues(testIssues, config)).toEqual(
-    testIssues
-      .split(' ')
-      .map((issue) => url + '/' + issue)
-      .join(', ')
-  );
+  expect(lib.formatIssues(testIssues, config))
+    .toEqual(
+      prefix + testIssues
+        .split(' ')
+        .map((issue) => url + '/' + issue)
+        .join(', ')
+    );
 })
 
 test('returns issues numbers prepended with a prefix as a valid url (in query params)', () => {
-  const url = 'https://test.com?jql='
-  const { config } = setup({
+  const url = 'https://test.com?jql=';
+  const { config, prefix } = setup({
     issuesPrefix: url
   })
   const testIssues = '13123-asf123 123123 ass1jJiasd123';
-  expect(lib.formatIssues(testIssues, config)).toEqual(
-    testIssues
-      .split(' ')
-      .map((issue) => url.split('?').join('/?') + issue)
-      .join(', ')
-  );
+  expect(lib.formatIssues(testIssues, config))
+    .toEqual(
+      prefix + testIssues
+        .split(' ')
+        .map((issue) => url.split('?').join('/?') + issue)
+        .join(', ')
+    );
 })
