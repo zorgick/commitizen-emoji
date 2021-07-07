1270953f90bebb9e806b92c450d0e38494a14518
Zackary Kononov <zorgizorgik@gmail.com>
2021-07-07 22:25:08 +0300

    🏷️  types: add types for formatters

--- src/types/typesFormatters.ts
+++ src/types/typesFormatters.ts
@@ -1,3 +1,4 @@
+import { Answers } from 'inquirer';
 import {
   ConfigType,
 } from './index';
@@ -15,6 +16,9 @@ export type AnswersHashesType = {
   issues: string;
 };
 export type FormatTitleAnwersType = Pick<AnswersHashesType, 'type' | 'scope' | 'subject'>;
+export type FinalFormatAnwersType =
+  Pick<AnswersHashesType, 'subject'>
+  & Partial<Pick<AnswersHashesType, 'body' | 'breakingBody' | 'issues'>>;
 
 // Functions
 
@@ -31,5 +35,9 @@ export type FormatCommitBodyType = (
 ) => string;
 export type FormatIssuesType = (
   issues: string,
+  config: ConfigType,
+) => string;
+export type FinalFormatType = (
+  answers: FinalFormatAnwersType,
   config: ConfigType,
 ) => string;
