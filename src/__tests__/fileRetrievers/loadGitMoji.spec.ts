df43f80591e3334d2314959a59976bd12366ae3a
Zackary Kononov <zorgizorgik@gmail.com>
2021-07-09 23:13:35 +0300

    🧪  fall(tdd): add case for a missing directory for loadGitmoji

--- src/__tests__/fileRetrievers/loadGitMoji.spec.ts
+++ src/__tests__/fileRetrievers/loadGitMoji.spec.ts
@@ -1,14 +1,43 @@
 import path from 'path';
 import fs from 'fs';
-import util from 'util';
 import lib from 'lib';
 
 import testFile from '../fixtures/testFile.json';
 
-const unlink = util.promisify(fs.unlink);
-const pathToGimojis = path.join(__dirname, '..', '..', 'data', 'gitmojis.json')
+const setup = () => {
+  const rm = fs.promises.rm;
+  const unlink = fs.promises.unlink;
+  const pathToGimojis = path.join(__dirname, '..', '..', 'data', 'gitmojis.json')
+  const pathToGimojisDir = path.dirname(pathToGimojis);
+
+  return {
+    unlink,
+    rm,
+    pathToGimojis,
+    pathToGimojisDir,
+  }
+}
+
+test('creates directory, if it doesn\'t exist yet', async () => {
+  const {
+    rm,
+    pathToGimojisDir,
+  } = setup();
+  if (fs.existsSync(pathToGimojisDir)) {
+    await rm(pathToGimojisDir, { recursive: true });
+  }
+  expect(fs.existsSync(pathToGimojisDir)).toEqual(false);
+
+  await lib.loadGitmoji();
+
+  expect(fs.existsSync(pathToGimojisDir)).toEqual(true);
+});
 
 test('loads via network (mock) missing gitmojis file', async () => {
+  const {
+    unlink,
+    pathToGimojis,
+  } = setup();
   // remove file, if it exists
   if (fs.existsSync(pathToGimojis)) {
     await unlink(pathToGimojis);
@@ -22,6 +51,10 @@ test('loads via network (mock) missing gitmojis file', async () => {
 });
 
 test('loads file from local fs', async () => {
+  const {
+    unlink,
+    pathToGimojis,
+  } = setup();
   expect(fs.existsSync(pathToGimojis)).toEqual(true);
   const result = await lib.loadGitmoji();
 
